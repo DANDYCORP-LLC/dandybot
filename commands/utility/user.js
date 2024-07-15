398fda664518d64d1dfd3306b6f8e2a2e289c518
dandy <thederpcow@gmail.com>
2024-07-15 17:27:22 -0600

    created executes for 3 util commands

---
+++ commands/utility/user.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('user')
+        .setDescription('gives info about you'),
+    async execute(interaction) {
+        await interaction.reply(`username is ${interaction.user.username}, join date is ${interaction.member.joinedAt}.`);
+    },
+};
